5745f6ae540f6ca60e84c9e9e125984170b54838
Sebastião Fortes <sebastiaofortes4@gmail.com>
2021-12-30 17:24:00 -0300

    Update MainController.java

--- src/main/java/com/blockchain/Controller/MainController.java
+++ src/main/java/com/blockchain/Controller/MainController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -143,7 +144,7 @@ public String Test() {
 @GetMapping(path="/dell_node") 
 	  public @ResponseBody String dellNode(@RequestParam String nome) {
 
-	noderepository.delete(nome);
+	noderepository.deleteById(nome);
 	System.out.println("Relacionamentos excluídos ");
 	  
 	 return "Dados deletados com sucesso!";	  
